114a86a937c0c3f4906214329e976fe9f7ecdbc2
alex <arodriguez202020@gmail.com>
2021-09-28 19:57:54 -0500

    add axios and default baseURL,  create home page to index products

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>All Products</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <img :src="product.image_url" alt="product.name" />
+      <h2>{{ product.name }}</h2>
+      <p>{{ product.price }}</p>
+      <p>{{ product.description }}</p>
+    </div>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("/products").then((response) => {
+        console.log("products index", response);
+        this.products = response.data;
+      });
+    },
   },
 };
 </script>
