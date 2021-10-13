9c53c293e5728b8c2abe92f9f8d3b13413c577c9
alex <arodriguez202020@gmail.com>
2021-10-12 22:01:36 -0500

    remove extra html theme pages and delete commented code on orders page

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -170,26 +170,6 @@
       </section>
       <!--================ confirmation part end =================-->
     </main>
-    <!-- <h1>Users Ordered Products</h1>
-    <div v-for="order in orders" :key="order.id">
-      <h2>Order ID:</h2>
-      <p>{{ order.id }}</p>
-      <p>Subtotal:</p>
-      <p>{{ order.money_math[0] }}</p>
-      <p>Tax:</p>
-      <p>{{ order.money_math[1] }}</p>
-      <p>Total:</p>
-      <p>{{ order.money_math[2] }}</p>
-      <div v-for="product in order.products" :key="product.id">
-        <h2>{{ product.name }}</h2>
-        <img :src="product.image_url" :alt="product.name" />
-        <p>{{ product.price }}</p>
-        <div v-for="quantity in order.carted_products" :key="quantity.id">
-          <p v-if="quantity.product_id == product.id">Quantity: {{ quantity.quantity }}</p>
-        </div>
-        <router-link v-bind:to="`/products/${product.id}`">More details</router-link>
-      </div>
-    </div> -->
   </div>
 </template>
 
